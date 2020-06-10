71dbdb3919ad14bf4abb810a1e6a81c541af1b8c
Rebecca Ceppas <receppasdc@gmail.com>
2020-06-10 12:42:57 -0400

    done linear regression

---
+++ regression.py
@@ -0,0 +1,71 @@
+import numpy as np
+import scipy.stats as ss
+import matplotlib.pyplot as plt
+
+n = 100
+beta_0 = 5
+beta_1 = 2
+np.random.seed(1)  # to get same values as the course
+
+x = 10 * ss.uniform.rvs(size=n)
+y = beta_0 + beta_1 * x + ss.norm.rvs(loc=0, scale=1, size=n)
+
+plt.figure()
+plt.plot(x,y,'o',ms=5)
+xx = np.array([0,10])
+plt.plot(xx, beta_0 + beta_1 * xx)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.savefig('regression1.jpg')
+
+rss = []
+slopes = np.arange(-10,15,0.01)
+for slope in slopes:
+    rss.append(np.sum((y - beta_0 - slope * x) ** 2))
+
+ind_min = np.argmin(rss)
+print('Estimate for the slope: ', slopes[ind_min])
+
+plt.figure()
+plt.plot(slopes, rss)
+plt.xlabel('Slope')
+plt.ylabel('RSS')
+plt.savefig('RSSestimate.jpg')
+
+import statsmodels.api as sm
+
+X = sm.add_constant(x)
+mod = sm.OLS(y, X)
+est = mod.fit()
+print(est.summary())
+
+n = 500
+beta_0 = 5
+beta_1 = 2
+beta_2 = -1
+np.random.seed(1)
+x_1 = 10 * ss.uniform.rvs(size=n)
+x_2 = 10 * ss.uniform.rvs(size=n)
+y = beta_0 + beta_1 * x_1 + beta_2 * x_2 + ss.norm.rvs(loc=0, scale=1, size=n)
+
+X = np.stack([x_1, x_2], axis=1)
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0], X[:,1], y, c=y)
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+ax.set_zlabel('$y$')
+
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression(fit_intercept=True)
+lm.fit(X, y)
+print(lm.intercept_)
+print(lm.coef_)
+
+X_0 = np.array([2, 4])
+print(lm.predict(X_0.reshape(1,-1)))
+
+print(lm.score(X, y))
